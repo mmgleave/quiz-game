eb2b32e9d580fad5b7db29ed5ee6efb04662f35a
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-04-10 15:02:32 -0600

    loop through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,107 @@
- // questions array
- var questionsArray = [
-    { q: "Question 1", a1: "Answer 1", a2: "Answer 2", a3: "Answer 3", a4: "Answer 4", a5: "Answer 1"},
+// hide questions content before quiz begins
+window.onload = function () {
+    document.getElementById("main-container").style.display = "none";
+}
+
+// questions array
+var questionsArray = [
+    {
+        q: "What is my favorite color?",
+        a: {
+            a: "Red",
+            b: "Green",
+            c: "Blue",
+            d: "Gray",
+            correct: "Gray"
+        },
+    },
+    {
+        q: "What is my favorite movie?",
+        a: {
+            a: "In Bruges",
+            b: "Pretty Woman",
+            c: "The Mighty Ducks",
+            d: "Star Trek",
+            correct: "In Bruges"
+        }
+    },
+    {
+        q: "What is my favorite food?",
+        a: {
+            a: "Pizza",
+            b: "B",
+            c: "C",
+            d: "D",
+            correct: "Pizza"
+        }
+    }
 ];
+
+
+$("#btn-start").on('click', function () {
+    quizBegin();
+});
+
 
 // score variable
 var score = 0;
 
-window.onload = function(){
-    document.getElementById("main-container").style.display = "none";
-}
+var currentQ = 0;
 
-$("#btn-start").on('click', function(){
-    alert("The quiz is starting!");
-    quizBegin();
-});
+var selectedAnswer = "";
+console.log(selectedAnswer);
 
 // main quiz function (when start button is clicked)
-quizBegin = function() {
-    // display question main-container
-    document.getElementById("main-container").style.display = "block";
-   
-    // for loop to display questions
-    for (var i = 0; i < questionsArray.length; i++) {
-        // display current question and answer options to user
-        $("#question-text").replaceWith("Question: " + questionsArray[i].q);
-        $("#btn-ans1").text(questionsArray[i].a1);
-        $("#btn-ans2").text(questionsArray[i].a2);
-        $("#btn-ans3").text(questionsArray[i].a3);
-        $("#btn-ans4").text(questionsArray[i].a4);
+var quizBegin = function () {
 
-        var correctAnswer = questionsArray[i].a5;
+    for (i = 0; i < questionsArray.length; i++) {
+        // display question main-container
+        document.getElementById("main-container").style.display = "block";
 
-        $(".answer-btn").click(function(){
-            if($(this).text() === correctAnswer ) {
-                console.log("correct");
-                score++;
-            } else {
-                console.log("incorrect");
-            };
-        $("#score-value").replaceWith("Score: " + score);
-        });
+        // display question 
+        $("#question-text").text(questionsArray[currentQ].q);
 
-      }
+        $("#btn-ans1").text(questionsArray[currentQ].a.a);
+        $("#btn-ans2").text(questionsArray[currentQ].a.b);
+        $("#btn-ans3").text(questionsArray[currentQ].a.c);
+        $("#btn-ans4").text(questionsArray[currentQ].a.d);
+    };
+    
+    verifyAnswer();
 };
+
+var verifyAnswer = function () {
+    // selected answer
+    $(".answer-btn").on("click", function () {
+        selectedAnswer = $(this).text();
+        console.log(selectedAnswer);
+        console.log(questionsArray[currentQ].a.correct);
+        if (selectedAnswer === questionsArray[currentQ].a.correct) {
+            console.log("CORRECT");
+            currentQ++;
+            score++;
+            $("#score-value").text(score);
+        } else {
+            currentQ++;
+            console.log("INCORRECT");
+            $("#score-value").text(score);
+        }
+        selectedAnswer = "";
+
+        if(currentQ < questionsArray.length) {
+        // display question 
+        $("#question-text").text(questionsArray[currentQ].q);
+
+        $("#btn-ans1").text(questionsArray[currentQ].a.a);
+        $("#btn-ans2").text(questionsArray[currentQ].a.b);
+        $("#btn-ans3").text(questionsArray[currentQ].a.c);
+        $("#btn-ans4").text(questionsArray[currentQ].a.d);
+        } else {
+            endQuiz();
+        }
+    });
+};
+
+var endQuiz = function() {
+    document.getElementById("main-container").style.display = "none";
+}
